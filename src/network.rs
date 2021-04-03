831029852a7c4fecb25624ef842937b917891767
Pjottos <35270305+Pjottos@users.noreply.github.com>
2021-04-03 12:38:25 +0200

    add methods for accessing network params and a constructor taking network params

--- src/network.rs
+++ src/network.rs
@@ -1,6 +1,6 @@
 use rand::prelude::*;
 
-use std::iter;
+use std::{iter, num::NonZeroUsize};
 
 pub struct Network {
     accumulators: Vec<ActionPotential>,
@@ -12,22 +12,20 @@ pub struct Network {
 }
 
 #[derive(Clone, Copy, Debug, PartialEq, PartialOrd, Eq, Ord, Default)]
-struct ActionPotential(i32);
+pub struct ActionPotential(pub i32);
 
 #[derive(Clone, Copy, Debug, PartialEq, PartialOrd, Eq, Ord, Default)]
-struct Effect(i8);
+pub struct Effect(pub i8);
 
 impl Network {
-    pub fn new(neuron_count: usize, connection_range: usize) -> Self {
-        assert!(neuron_count > 0);
-        assert!(connection_range > 0);
-        let effect_count = neuron_count.checked_mul(connection_range)
+    pub fn new(neuron_count: NonZeroUsize, connection_range: NonZeroUsize) -> Self {
+        let effect_count = neuron_count.get().checked_mul(connection_range.get())
             .expect("neuron_count or connection_range too big");
 
         let mut rng = thread_rng();
 
         let action_potentials = iter::repeat_with(|| ActionPotential(rng.gen()))
-            .take(neuron_count)
+            .take(neuron_count.get())
             .collect();
 
         let effects = iter::repeat_with(|| Effect(rng.gen()))
@@ -35,11 +33,42 @@ impl Network {
             .collect();
         
         Self {
-            accumulators: vec![Default::default(); neuron_count as usize],
+            accumulators: vec![Default::default(); neuron_count.get()],
+            connection_range: connection_range.get(),
+            
+            action_potentials,
+            effects,
+        }
+    }
+
+    pub fn with_params(action_potentials: Vec<ActionPotential>, effects: Vec<Effect>) -> Self {
+        let neuron_count = action_potentials.len();
+        assert!(neuron_count > 0);
+        let connection_range = effects.len() / neuron_count;
+        assert!(connection_range > 0);
+
+        Self {
+            accumulators: vec![Default::default(); neuron_count],
             connection_range,
             
             action_potentials,
             effects,
         }
+    }
+
+    pub fn action_potentials(&self) -> &[ActionPotential] {
+        &self.action_potentials
+    }
+
+    pub fn action_potentials_mut(&mut self) -> &mut [ActionPotential] {
+        &mut self.action_potentials
+    }
+    
+    pub fn effects(&self) -> &[Effect] {
+        &self.effects
+    }
+
+    pub fn effects_mut(&mut self) -> &mut [Effect] {
+        &mut self.effects
     }
 }
